b3fe458f24e38483bf3f83ebc91ef67e236747c1
CedricHommann <80926047+CedricHommann@users.noreply.github.com>
2024-04-05 08:19:34 +0200

    .

--- ios/takeMeHomeWidget/types.swift
+++ ios/takeMeHomeWidget/types.swift
@@ -8,13 +8,31 @@
 import Foundation
 import WidgetKit
 
-struct RouteInformation: TimelineEntry
+struct RouteInformation: TimelineEntry, Codable
 {
     let date: Date
     let route: [RoutePart]
+    
+    static func fromJson(jsonString: String) -> RouteInformation?
+    {
+        var jsonData = jsonString.data(using: .utf8)
+        
+        let decoder = JSONDecoder()
+        decoder.dateDecodingStrategy = .iso8601
+        
+        do {
+            let routeInformation = try decoder.decode(RouteInformation.self, from: jsonData!)
+            print(routeInformation)
+            return routeInformation
+        } catch {
+            print(error)
+        }
+        
+        return nil
+    }
 }
 
-struct RoutePart
+struct RoutePart: Codable
 {
     let vehicle: VehicleType
     let lineName: String
@@ -23,13 +41,31 @@ struct RoutePart
     let entranceTime: Date
     let exit: String
     let exitTime: Date
+    
+    static func fromJson(jsonString: String) -> RoutePart?
+    {
+        var jsonData = jsonString.data(using: .utf8)
+            
+        let decoder = JSONDecoder()
+        decoder.dateDecodingStrategy = .iso8601
+        
+        do {
+            let routePart = try decoder.decode(RoutePart.self, from: jsonData!)
+            print(routePart)
+            return routePart
+        } catch {
+            print(error)
+        }
+        
+        return nil
+    }
 }
 
-enum VehicleType
+enum VehicleType: String, Codable
 {
-  case unknown
-  case walk
-  case tram
-  case train
-  case bus
+  case unknown = "unknown"
+  case walk = "walk"
+  case tram = "tram"
+  case train = "train"
+  case bus = "bus"
 }
