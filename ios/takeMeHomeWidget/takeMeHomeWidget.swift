6d60918cc8658ac06e2670ebd92b0b4dbe25b03e
CedricHommann <80926047+CedricHommann@users.noreply.github.com>
2024-04-07 15:45:29 +0200

    comments deleted

--- ios/takeMeHomeWidget/takeMeHomeWidget.swift
+++ ios/takeMeHomeWidget/takeMeHomeWidget.swift
@@ -49,60 +49,6 @@ struct Provider: TimelineProvider
             let timeline = Timeline(entries: [entry], policy: .atEnd)
             completion(timeline)
         }
-        
-        /*
-        var entries: [RouteInformation] = []
-
-        // Generate a timeline consisting of five entries an hour apart, starting from the current date.
-        let currentDate = Date()
-        for hourOffset in 0 ..< 5
-        {
-            // let entryDate = Calendar.current.date(byAdding: .hour, value: hourOffset, to: currentDate)!
-            let entry = RouteInformation(date: Date(timeIntervalSince1970: 1708526700),
-                                         route:
-                                            [
-                                                RoutePart(vehicle: VehicleType.tram,
-                                                          lineName: "3",
-                                                          lineDestination: "Friedrich Engels Straße",
-                                                          entrance: "DHGE",
-                                                          entranceTime: Date(timeIntervalSince1970: 1708523280),
-                                                          exit: "Friedrich Engels Straße",
-                                                          exitTime: Date(timeIntervalSince1970: 1708524000)),
-                                                RoutePart(vehicle: VehicleType.walk,
-                                                          lineName: "",
-                                                          lineDestination: "",
-                                                          entrance: "Friedrich Engels Straße",
-                                                          entranceTime: Date(timeIntervalSince1970: 1708524000),
-                                                          exit: "Gera Hbf",
-                                                          exitTime: Date(timeIntervalSince1970: 1708524300)),
-                                                RoutePart(vehicle: VehicleType.train,
-                                                          lineName: "RE1",
-                                                          lineDestination: "Göttingen",
-                                                          entrance: "Gera Hbf",
-                                                          entranceTime: Date(timeIntervalSince1970: 1708524300),
-                                                          exit: "Erfurt Hbf",
-                                                          exitTime: Date(timeIntervalSince1970: 1708526700)),
-                                                RoutePart(vehicle: VehicleType.tram,
-                                                          lineName: "5",
-                                                          lineDestination: "Zoopark",
-                                                          entrance: "Erfurt Hbf",
-                                                          entranceTime: Date(timeIntervalSince1970: 1708526700),
-                                                          exit: "Lutherkirche",
-                                                          exitTime: Date(timeIntervalSince1970: 1708527300)),
-                                                RoutePart(vehicle: VehicleType.walk,
-                                                          lineName: "",
-                                                          lineDestination: "",
-                                                          entrance: "Lutherkirche",
-                                                          entranceTime: Date(timeIntervalSince1970: 1708527300),
-                                                          exit: "Schobersmühlenweg",
-                                                          exitTime: Date(timeIntervalSince1970: 1708527600)),
-                                            ])
-            entries.append(entry)
-        }
-
-        let timeline = Timeline(entries: entries, policy: .atEnd)
-        completion(timeline)
-        */
     }
 }
 
